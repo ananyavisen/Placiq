d78ab8da7030cf7f158ee3767d1972337c8171f6
Ananya Visen <ananvisen@gmail.com>
2026-08-06 16:29:18 +0530

    Bg fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import Login from './Components/Login/Login'
+import bg from "./assets/bg.png";
 
 function App() {
   return (
     <>
+     <div
+        className="min-h-screen bg-cover bg-center bg-no-repeat p-5 sm:p-4 lg:p-4"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
       <Login />
+      </div>
     </>
   )
 }
