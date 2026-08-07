c9862fd9296c676081b6ad421aaa3c3d9fd9a3b0
ananya-ag286 <ananyaagrawal286@gmail.com>
2026-08-07 23:41:23 +0530

    Fix resources page entry and shared components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import Login from './Components/Login/Login'
+import Resources from "./Components/Resources/Resources";
 import bg from "./assets/bg.png";
 
 function App() {
   return (
-    <>
-     <div
-        className="min-h-screen bg-cover bg-center bg-no-repeat p-5 sm:p-4 lg:p-4"
-        style={{ backgroundImage: `url(${bg})` }}
-      >
-      <Login />
-      </div>
-    </>
-  )
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat p-5 sm:p-4 lg:p-4"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <Resources />
+    </div>
+  );
 }
 
-export default App
+export default App;
