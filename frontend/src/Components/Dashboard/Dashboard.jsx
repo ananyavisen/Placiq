3c531d6672bfdf628640c91773069d8d7a4e8962
Anekvarna <anek.varna030@gmail.com>
2026-08-08 14:18:09 +0530

    Complete dashboard right section

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import Topbar from "./Topbar";
 import Welcome from "./Welcome";
 import StatsGrid from "./StatsGrid";
 import QuickActions from "./QuickActions";
+import Schedule from "./Schedule";
 
 import bg from "../../assets/bg.png";
 
@@ -20,8 +21,20 @@ export default function Dashboard() {
         <div className="flex-1">
           <Topbar />
           <Welcome />
+            <div className="grid grid-cols-1 lg:grid-cols-[2.25fr_1fr] gap-5">
+
+           {/* Left section */}
+          <div className="space-y-5">
           <QuickActions />
-           <StatsGrid />
+          <StatsGrid />
+          </div>
+
+           {/* Right section */}
+          {/* <div className="lg:col-span-1"> */}
+          <div>
+           <Schedule />
+        </div>
+      </div>
         </div>
       </div>
     </div>
